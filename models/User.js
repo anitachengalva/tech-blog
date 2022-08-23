9215ec2e06e63e3208f8ffcaafe8ccfa7629fcf2
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-23 13:22:48 -0700

    extended model class with user and added password check

--- models/User.js
+++ models/User.js
@@ -1,3 +1,9 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection.js");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
